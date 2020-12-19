1b2a9494dae6d189b6af1010b7c0526e16928132
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-12-19 16:14:55 +0900

    cs app start

---
+++ cs-console-app/cs-console-app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace cs_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 11;
+            Console.WriteLine("{0}", x);
+            
+        }
+    }
+}
