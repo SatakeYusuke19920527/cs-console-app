de5f33d1ee0e9abd9cb080de55aa11e029414d54
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-12-20 09:51:47 +0900

    2日t目

--- cs-console-app/cs-console-app/Program.cs
+++ cs-console-app/cs-console-app/Program.cs
@@ -6,9 +6,14 @@ namespace cs_console_app
     {
         static void Main(string[] args)
         {
-            int x = 11;
-            Console.WriteLine("{0}", x);
-            
+            int x = 3;
+            int y = 7;
+
+            x++;
+            y--;
+            Console.WriteLine("x={0},y={1}", x, y);
+            Console.WriteLine("2日目 参考書買おうかなぁ")
+
         }
     }
 }
